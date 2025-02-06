d631054325c1643efc97219279e9c9afb546d30f
Sinclair Nzenwata <83012040+starksinclair@users.noreply.github.com>
2025-02-06 01:31:55 -0500

    added new tests

--- src/test/java/edu/bsu/cs/RevisionParserTest.java
+++ src/test/java/edu/bsu/cs/RevisionParserTest.java
@@ -1,44 +1,44 @@
 package edu.bsu.cs;
 
 import com.jayway.jsonpath.JsonPath;
-import org.junit.jupiter.api.BeforeEach;
+import edu.bsu.cs.utils.RevisionParser;
 import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class RevisionParserTest {
-
-//    @BeforeEach
-//    void setUp() throws IOException {
-//        jsonResponse = new String(Files.readAllBytes(Paths.get("src/test/assets/sample.json")));
-//    }
-
+public class RevisionParserTest {
+    RevisionParser revisionParser = new RevisionParser("src/test/resources/sample.json");
     @Test
-    public void testExtractFirstUser() throws IOException {
-        String jsonResponse = readSampleFileAsString();
-        List<String> users = JsonPath.read(jsonResponse, "$..revisions[*].user");
+    public void ExtractFirstUserTest() throws IOException {
+        List<String> users = revisionParser.extractUsers();
         assertFalse(users.isEmpty());
         System.out.println("First editor: " + users.get(0));
-        assertNotNull(users.get(0)); // Ensures the first user exists
+        assertNotNull(users.get(0));
+    }
+    @Test
+    void ExtractTimestampsFromJsonTest() throws IOException {
+        List<String> timestamps = revisionParser.extractTimestamps();
+        assertEquals(4, timestamps.size());
+        assertEquals("2025-01-30T02:39:05Z", timestamps.get(0));
+        assertEquals("2025-01-12T11:36:41Z", timestamps.get(1));
+        assertEquals("2024-10-28T08:31:58Z", timestamps.get(2));
+        assertEquals("2024-10-24T13:10:57Z", timestamps.get(3));
+    }
+    @Test
+    void RedirectedArticleTest() throws IOException {
+        List<String> redirects = revisionParser.extractRedirects();
+        assertFalse(redirects.isEmpty());
+        assertEquals("General officer", redirects.get(0));
+    }
+    @Test
+    void MissingPageTest() throws IOException {
+        List<String> missing = revisionParser.extractMissingPages();
+        assertTrue(missing.isEmpty());
     }
 
-    private String readSampleFileAsString() throws NullPointerException, IOException {
-        try (InputStream sampleFile = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("sample.json")) {
-            return new String(Objects.requireNonNull(sampleFile).readAllBytes(), Charset.defaultCharset());
-        }catch (Exception error) {
-            throw new RuntimeException(error);
-        }
-    }
 
-//    private String hh() {
-//        Object o = new JSONParser
-//    }
 }
