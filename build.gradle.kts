63790a0a5980b4817c76209a2c250498f95cfd90
Sinclair Nzenwata <83012040+starksinclair@users.noreply.github.com>
2025-02-09 22:04:00 -0500

    add a dependency to ignore the slf4j-nop errors.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     // for installing JSONPATH
     implementation("com.jayway.jsonpath:json-path:2.9.0")
-
+    // Added to ignore the slf4j-nop errors.
+    implementation("org.slf4j:slf4j-nop:2.0.9")
 }
 
 tasks.test {
